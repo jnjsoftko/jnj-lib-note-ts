af62429394d7f1c6a3038f7cd4e97758d6b5ec1f
Jnj Soft Ko <jnjsoft.ko@gmail.com>
2024-06-02 08:33:17 +0900

    notion.ts

--- README.md
+++ README.md
@@ -13,3 +13,22 @@ $ bootapp -u jnjsoftko -n jnj-lib-note-ts -d "JnJ Utility Library For Note(notio
 # npm install
 npm i @notionhq/client jnj-lib-base
 ```
+
+## Publish
+
+> `package.json`
+
+```json
+// AFTER
+  "name": "jnj-lib-note",
+
+// BEFORE
+  "name": "jnj-lib-note-ts",
+```
+
+```sh
+# github commit & push(VSCode)
+
+# publish
+$ publish
+```
--- src/notion.ts
+++ src/notion.ts
@@ -4,8 +4,7 @@ import { loadJson, saveJson } from "jnj-lib-base";
 import dotenv from "dotenv";
 dotenv.config(); // 실행 경로에 있는 `.env`
 
-const settingsPath =
-  process.env.ENV_SETTINGS_PATH ?? "C:/JnJ-soft/Developments/_Settings";
+const settingsPath = process.env.DEV_SETTINGS ?? "C:/JnJ-soft/Developments/_Settings";
 
 class Notion {
   token: string;
@@ -14,9 +13,7 @@ class Notion {
   // & CONSTRUCTOR
   // <name>_<sn>.json   ex: monblue_0.json
   constructor(name: string = "monblue", sn: number = 0) {
-    this.token = loadJson(
-      Path.join(settingsPath, `Apis/notion/${name}_${sn}.json`)
-    ).token;
+    this.token = loadJson(Path.join(settingsPath, `Apis/notion/${name}_${sn}.json`)).token;
     this.client = new Client({
       auth: this.token,
     });
